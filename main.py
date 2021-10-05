ac78a1f218b8676a0c5006ba53cf3464e1b22221
tudorsuiu <tudorsuiu21@gmail.com>
2021-10-05 22:14:42 +0300

    Modificare efectuata cu succes.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def is_antipalindrome(n):
-    '''
+    """
     Determina daca un numar n este antipalindrom
     :param n: nr. intreg
     :return: True daca n este antipalindrom si False in caz contrar
-    '''
-
-    n = abs(n)
+    """
+    # Un numar negativ nu este niciodata palindrom
+    if n < 0:
+        return True
     antipalindrom = True
     cn = n
     nr_cifre = 0
-
     # Determinam cate cifre are numarul citit
     while cn != 0:
         nr_cifre += 1
@@ -21,31 +21,23 @@ def is_antipalindrome(n):
         n = n % (10 ** (nr_cifre - 1))
         n = n // 10
         nr_cifre -= 2
-
     return antipalindrom
 
+
 def test_is_antipalindrome():
-    assert is_antipalindrome(-1) == True
-    assert is_antipalindrome(0) == True
-    assert is_antipalindrome(5) == True
-    assert is_antipalindrome(11) == False
-    assert is_antipalindrome(21) == True
-    assert is_antipalindrome(353) == False
-    assert is_antipalindrome(335) == True
-    assert is_antipalindrome(2773) == False
-    assert is_antipalindrome(2783) == True
-    assert is_antipalindrome(27733) == True
-    assert is_antipalindrome(27373) == False
+    assert is_antipalindrome(-1) is True
+    assert is_antipalindrome(0) is True
+    assert is_antipalindrome(2773) is False
+    assert is_antipalindrome(2783) is True
+
 
 def get_base_2(n: str):
-    '''
+    """
     Transforma un numar dat din baza 10 in baza 2
     :param n: sir de caractere
     :return: numarul n transformat in baza 2
-    '''
-
+    """
     n_base_2 = ""
-
     if int(n) == 0:
         return "0"
     elif int(n) < 0:
@@ -56,6 +48,7 @@ def get_base_2(n: str):
             n = str(int(n) // 2)
         return n_base_2
 
+
 def test_get_base_2():
     assert get_base_2("-1") == "Numar invalid!"
     assert get_base_2("0") == "0"
@@ -64,11 +57,10 @@ def test_get_base_2():
     assert get_base_2("3") == "11"
     assert get_base_2("4") == "100"
 
+
 def main():
-
-    shouldRun = True
-
-    while shouldRun:
+    should_run = True
+    while should_run:
         print("1.Determina daca un numar este antipalindrom")
         print("2.Transforma un numar dat din baza 10 in baza 2")
         print("x.Iesire")
@@ -80,9 +72,10 @@ def main():
             n = int(input("Dati numarul: "))
             print(get_base_2(str(n)))
         elif optiune == "x":
-            shouldRun = False
+            should_run = False
         else:
             print("Optiune gresita! Reincercati!")
+
 
 test_is_antipalindrome()
 test_get_base_2()
